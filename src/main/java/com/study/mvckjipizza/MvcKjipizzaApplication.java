f434cfb39d5c3d2fffc3d80fdb8a73db716c0679
hajaeryul <wofbf2@gmail.com>
2022-11-14 16:05:45 +0900

    스프링 프로젝트 생성

---
+++ src/main/java/com/study/mvckjipizza/MvcKjipizzaApplication.java
@@ -0,0 +1,13 @@
+package com.study.mvckjipizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcKjipizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcKjipizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/mvckjipizza/MvcKjipizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.mvckjipizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcKjipizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
