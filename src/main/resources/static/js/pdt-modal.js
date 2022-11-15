fd40a92d294fde9d08d36d1fadd21361600b33ee
syw1114 <tkdvlftj11@naver.com>
2022-11-15 17:53:28 +0900

    모달작업중

--- src/main/resources/static/js/pdt-modal.js
+++ src/main/resources/static/js/pdt-modal.js
@@ -6,6 +6,21 @@ document.querySelector(".modal-close-btn").onclick = () => {
     document.querySelector(".modal-container").classList.add("hidden");
 }
 
-document.querySelector(".bg").onclick = () => {
+document.querySelector(".bg").onclick =() => {
     document.querySelector(".modal-container").classList.add("hidden");
 }
+
+const modalCart = document.querySelector(".modal-cart");
+const cartOpen = document.querySelector(".bag-btn");
+const cartClose = document.querySelector(".cart-close");
+const cartBody = document.querySelector(".cart-body");
+
+cartOpen.onclick = () => {
+    modalCart.classList.remove("hidden");
+}
+
+
+
+
+
+
