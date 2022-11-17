081b7a580354fba31539f931f9de357303d1f9c1
syw1114 <tkdvlftj11@naver.com>
2022-11-17 16:58:02 +0900

    중간작업중

---
+++ src/main/java/com/study/mvckjipizza/api/ProductApi.java
@@ -0,0 +1,25 @@
+package com.study.mvckjipizza.api;
+
+
+import com.study.mvckjipizza.dto.CMRespDto;
+import com.study.mvckjipizza.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class ProductApi {
+
+    private final ProductService productService;
+
+    @GetMapping("/products/{category}")
+    public ResponseEntity<?> getPRoduct(@PathVariable String category) throws Exception {
+        return ResponseEntity.ok(new CMRespDto<>("Successfully",productService.getProductList(category)));
+
+    }
+}
---
+++ src/main/java/com/study/mvckjipizza/dto/CMRespDto.java
@@ -0,0 +1,11 @@
+package com.study.mvckjipizza.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class CMRespDto<T> {
+    private String msg;
+    private T data;
+}
