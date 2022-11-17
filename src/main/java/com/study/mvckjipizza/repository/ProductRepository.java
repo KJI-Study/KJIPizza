57b85e63304a40bd2c101d99ed7cd0932372e331
syw1114 <tkdvlftj11@naver.com>
2022-11-17 18:07:32 +0900

    no message

--- src/main/java/com/study/mvckjipizza/repository/ProductRepository.java
+++ src/main/java/com/study/mvckjipizza/repository/ProductRepository.java
@@ -1,10 +1,11 @@
 package com.study.mvckjipizza.repository;
 
 import com.study.mvckjipizza.domain.CollectionsProduct;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 import java.util.Map;
-
+@Mapper
 public interface ProductRepository {
 
     public List<CollectionsProduct> getProductList(Map<String,Object> map) throws Exception;
