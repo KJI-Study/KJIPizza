2b82077d7db0cb00f5708c973eb8c9ab9c1a9d14
kwak-daeun <kjkk126@NATE.COM>
2022-11-18 17:53:19 +0900

    category data ok. adminpage is updating ..

--- src/main/java/com/study/mvckjipizza/service/admin/ProductManagementService.java
+++ src/main/java/com/study/mvckjipizza/service/admin/ProductManagementService.java
@@ -1,10 +1,12 @@
 package com.study.mvckjipizza.service.admin;
 
+import com.study.mvckjipizza.dto.CategoryResponseDto;
 import com.study.mvckjipizza.dto.admin.ProductRegisterReqDto;
 
 import java.util.List;
 
 public interface ProductManagementService {
 
-    public List<ProductRegisterReqDto> getCategoryList() throws Exception;
+    public List<CategoryResponseDto> getCategoryList() throws Exception;
+
 }
--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -2,6 +2,42 @@ document.querySelector(".img-container").onclick = () => {
     document.querySelector(".file-input").click();
 }
 
+class ProductMst{ //앞에 #을 붙이는건 private이라는뜻
+    #category;
+    #name;
+    #price; 
+  
+    
+    constructor(category, name, price){
+        this.#category = category;
+        this.#name = name;
+        this.#price = price; 
+
+
+    }
+
+    getCategory() {return this.#category;}
+    setCategory(category) {this.#category = category;}   
+
+    getName() {return this.#name;}
+    setNmae(name) {this.#name =name;}   
+
+    getPrice() {return this.#price;}
+    setPrice(price) {this.#price =price;}   
+  
+
+    getObject(){ //parameter 요청할때 쓰는코드
+        const obj ={
+            category  : this.#category, 
+            name : this.#name,
+            price  : this.#price,
+         
+            
+        }
+        return obj;
+    }
+}
+
 class CommonApi {
     getCategoryList(){
         let responseResult = null;
@@ -12,11 +48,12 @@ class CommonApi {
             url: "/api/admin/product/category",
             dataType: "json",
             success: (response) => {
+                console.log(response.data);
                 responseResult = response.data;
             },
             error : (error) => {
                 console.log(error);
-            }
+j            }
 
         })
 
@@ -25,13 +62,88 @@ class CommonApi {
     }
 }
 
+class RegisterApi{
+    createProductRequest(ProductMst){
+        let responseResult = null;
+
+        $.ajax({
+            async : false,
+            type: "post",
+            url: "/api/admin/product",
+            contentType : "application/json",
+            dataType: "json",
+            success: (response) => {
+                responseResult = response.data;
+            },
+
+            error : (error) => {
+                console.log(error);
+            }
+        })
+
+        return responseResult;
+    }
+}
+
+class RegisterEventService{
+
+    #categorySelectObj;
+    #nameInputObj;
+    #priceInputObj;
+    #registButtonObj;
+
+    constructor() {
+        this.#categorySelectObj = document.querySelectorAll(".product-inputs")[0];
+        this.#nameInputObj = document.querySelectorAll(".product-inputs")[1];
+        this.#priceInputObj = document.querySelectorAll(".product-inputs")[2];
+        this.#registButtonObj = document.querySelectorAll(".pdt-regist-btn");
+
+        this.init();
+
+
+        //event 
+        this.addCategorySelectEvent();
+        this.addNameInputEvent();
+        this.addPriceInputEvent();
+        this.addRegistButtonEvent();
+
+    }
+
+    init(){
+        this.#nameInputObj.disabled = true;
+        this.#priceInputObj.disabled = true;
+        this.#registButtonObj.disabled = true;
+    }
+
+    addCategorySelectEvent(){
+        this.#categorySelectObj.onchange =() => {
+            if(this.#categorySelectObj.value != "none"){
+                this.#nameInputObj.disabled = false;
+            }else{
+                this.#nameInputObj.disabled = true;
+            }
+        }
+    }    
+    
+
+    addRegistButtonEvent() {
+        this.#registButtonObj.onclick = () => {
+            const category = this.#categorySelectObj.value;
+            const name = this.#nameInputObj.value;
+            const price = this.#priceInputObj.value;
+
+        }
+    }
+
+} 
 class RegisterService{
     static #instance=null;
 
     constructor() {
-        this.loadRegister();
-
-        }
+       this.loadRegister();
+    
+    }
+        
 
     static getInstance(){
         if(this.#instance == null){
@@ -41,26 +153,35 @@ class RegisterService{
         
     }
 
+    loadRegister(){
+        new RegisterEventService();
+    }
+
+    setRegisterHeaderEvent(){ /*what means*/
+        new RegisterEventService();
+
+    }
+
+
         
     getCategoryList() {
         const commonApi = new CommonApi();
         const productCategoryList = commonApi.getCategoryList();
 
-        const productCategory = document.querySelector(".pdt-regist");
+        const productCategory = document.querySelector(".regist-category-select");
         productCategory.innerHTML = '<option value="none">상품 종류</option>';
 
-        productCategoryList.forEach(categroy => {
+        productCategoryList.forEach(category => {
             productCategory.innerHTML += `
-            <option value="${categroy.id}">${category.name}></option>
+            <option value="${category.id}">${category.name}</option>
             
             `;
-            
         })
             
     }
 
 }
 
-window.onload =() => {
-    RegisterService.getInstance.getCategoryList();
+window.onload = () => {
+    RegisterService.getInstance().getCategoryList();
 }
